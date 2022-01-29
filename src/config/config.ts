f1982de25f8aba4af9854615e6c90775adad5864
hibik17 <hibiki@macbook>
2022-01-29 21:30:28 +0900

    APIkey追加

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,10 @@
 export const config = {
-  firebase: {},
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  },
 };
