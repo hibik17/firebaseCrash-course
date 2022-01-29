97f63d2788d02612f718a51ef5fa9db8e67d6c3e
hibik17 <hibiki@macbook>
2022-01-29 23:21:06 +0900

    file revise

---
+++ src/application.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Route, RouteComponentProps, Switch } from "react-router-dom";
+import AuthRoute from "./components/index";
+import routes from "./config/routes";
+
+export interface IApplicationProps {}
+
+const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+  return (
+    <div>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            render={(routeProps: RouteComponentProps<any>) => {
+              if (route.protected)
+                return (
+                  <AuthRoute>
+                    <route.component {...routeProps} />
+                  </AuthRoute>
+                );
+
+              return <route.component {...routeProps} />;
+            }}
+          />
+        ))}
+      </Switch>
+    </div>
+  );
+};
+
+export default Application;
